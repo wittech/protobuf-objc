d83e989d73326ff30594f9e203d63d69dcc5bba5
Alexey Khokhlov <alexeyxo@gmail.com>
2015-01-15 16:14:10 +0300

    regenerate tests

--- src/compiler/makeTests.sh
+++ src/compiler/makeTests.sh
@@ -2,3 +2,4 @@ protoc google/protobuf/unittest* --objc_out="./Tests";
 protoc  --plugin=/usr/local/bin/protoc-gen-objc google/protobuf/descriptor* --objc_out="./Tests";
 cp -f ./Tests/Unittest* ../runtime/Tests/;
 cp -f ./Tests/Descriptor* ../runtime/Classes/;
+rm -f ../runtime/Tests/*.{h,m};
